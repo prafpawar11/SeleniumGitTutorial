c5851b4d34471dae9ad83c6f7ef0a640d1dfd281
prafpawar11 <=>
2025-11-04 08:30:58 +0530

    Contact TC By Sonali

---
+++ src/main/java/pageLayer/ContactPage.java
@@ -0,0 +1,10 @@
+package pageLayer;
+
+public class ContactPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Contact Page TC by Sonali Automation test Engineer");
+	}
+
+}
